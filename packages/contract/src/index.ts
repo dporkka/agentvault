c08b61733280095a59bcd35c43493e57ce723ab7
David Porkka <37228537+dporkka@users.noreply.github.com>
2026-09-12 09:35:15 -0300

    feat(contract): export capability types

--- packages/contract/src/index.ts
+++ packages/contract/src/index.ts
@@ -4,4 +4,5 @@ export * from './client';
 export * from './knowledge';
 export * from './context';
 export * from './mutations';
+export * from './capabilities';
 export * from './knowledge-client';
